b172b2179fcfa2a8b095230ee3a3880bfa6229a7
W4sp24 <ethansoliven1@gmail.com>
2026-05-06 01:33:43 +0800

    Initial Commit, Draft Project Structure

---
+++ ICS2609_FINAL_PROJECT/src/java/dao/MySqlBusinessDAO.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+/**
+ *
+ * @author ethan
+ */
+public class MySqlBusinessDAO {
+    
+    
+    
+    // Data access layer for mySQL server
+    
+}
