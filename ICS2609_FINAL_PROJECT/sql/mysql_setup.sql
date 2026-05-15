a377178234589abfa7442029620a108f41064296
Mickaelaaa <alegriamikaela7@gmail.com>
2026-05-15 13:24:27 +0800

    a

--- ICS2609_FINAL_PROJECT/sql/mysql_setup.sql
+++ ICS2609_FINAL_PROJECT/sql/mysql_setup.sql
@@ -1,3 +1,4 @@
+-- Database 2: MySQL (Couse Management)
 -- Table 1: Main Course Info
 CREATE TABLE COURSES (
     courseCode VARCHAR(10) PRIMARY KEY,
@@ -29,3 +30,33 @@ CREATE TABLE ENROLLMENTS (
     CONSTRAINT fk_enroll_course FOREIGN KEY (courseCode) 
         REFERENCES COURSES(courseCode)
 );
+
+INSERT INTO COURSES (courseCode, courseName, instructor, availableSlots, createdDate, endDate) VALUES
+('CS101', 'Introduction to Computing', 'Dr. Alan Turing', 45, '2026-01-10', '2026-05-20'),
+('ITS312', 'Database Management Systems', 'Prof. Grace Hopper', 30, '2026-01-10', '2026-05-20'),
+('ETHICS2', 'Social Ethics & Professionalism', 'Dr. Jose Rizal', 50, '2026-01-12', '2026-05-22'),
+('HIST1872', 'Philippine History: Cavite Mutiny', 'Prof. Teodoro Agoncillo', 25, '2026-01-12', '2026-05-22'),
+('MATH402', 'Differential Equations', 'Dr. Leonhard Euler', 40, '2026-01-10', '2026-05-20'),
+('AI500', 'Artificial Intelligence Algorithms', 'Dr. Yann LeCun', 20, '2026-01-15', '2026-05-25');
+
+INSERT INTO COURSE_SCHEDULES (courseCode, startTime, endTime, daysOfWeek) VALUES
+('CS101', '08:00:00', '09:30:00', 'Mon, Wed, Fri'),
+('ITS312', '10:00:00', '12:00:00', 'Tue, Thu'),
+('ETHICS2', '13:00:00', '14:30:00', 'Mon, Wed, Fri'),
+('HIST1872', '15:00:00', '16:30:00', 'Tue, Thu'),
+('MATH402', '07:30:00', '09:00:00', 'Tue, Thu'),
+('AI500', '14:00:00', '17:00:00', 'Fri');
+
+INSERT INTO ENROLLMENTS (courseCode, username) VALUES
+('CS101', 'user01@mail.com'),
+('CS101', 'tester_a@demo.org'),
+('ITS312', 'ace_admin@corp.com'),
+('ITS312', 'm.rossi@tech.net'),
+('ETHICS2', 'visit.22@site.net'),
+('ETHICS2', 'hello_world@js.io'),
+('HIST1872', 'c.kent@daily.com'),
+('HIST1872', 'd.prince@themyscira.org'),
+('MATH402', 't.stark@stark.com'),
+('MATH402', 'p.parker@bugle.com'),
+('AI500', 'n.neo@matrix.net'),
+('AI500', 's.rogers@shield.gov');
--- ICS2609_FINAL_PROJECT/src/java/dao/MySqlBusinessDAO.java
+++ ICS2609_FINAL_PROJECT/src/java/dao/MySqlBusinessDAO.java
@@ -1,18 +1,43 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dao;
 
-/**
- *
- * @author ethan
- */
+import java.sql.*;
+import java.util.*;
+import javax.servlet.ServletContext;
+import model.Course;
+
 public class MySqlBusinessDAO {
-    
-    
-    
-    // Data access layer for mySQL server
-    
+    private String driver, url, user, pass;
+
+    public MySqlBusinessDAO(ServletContext context) {
+        this.driver = context.getInitParameter("MySQL_Driver");
+        this.url = context.getInitParameter("MySQL_URL");
+        this.user = context.getInitParameter("MySQL_User");
+        this.pass = context.getInitParameter("MySQL_Pass");
+    }
+
+    private Connection getConnection() throws Exception {
+        Class.forName(driver);
+        return DriverManager.getConnection(url, user, pass);
+    }
+
+    public List<Course> getAllCourses() {
+        List<Course> courseList = new ArrayList<>();
+        String query = "SELECT * FROM COURSES";
+
+        try (Connection conn = getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(query)) {
+
+            while (rs.next()) {
+                Course course = new Course();
+                course.setCourseID(rs.getInt("courseID"));
+                course.setCourseName(rs.getString("courseName"));
+                course.setUnits(rs.getInt("units"));
+                courseList.add(course);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return courseList;
+    }
 }
